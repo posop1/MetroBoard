729650384cbcc7c2483f011bc3ede13631df5a83
posop <artur.yakhin52@gmail.com>
2023-03-12 20:19:21 +0500

    add title

--- metroboard-client/src/components/Layout/Header.vue
+++ metroboard-client/src/components/Layout/Header.vue
@@ -1,17 +1,20 @@
 <template>
-  <v-app-bar
-    density="compact"
-    :elevation="8"
-  >
+  <v-app-bar flat>
     <v-toolbar-title>
       <v-img
         src="https://www.metro-set.ru/data/common/logo/logo.svg"
         aspect-ratio="1/1"
         width="100"
         height="40"
+        to="/"
       ></v-img>
     </v-toolbar-title>
-
+    <v-row>
+      <h2 class="text-h5">
+        <span style="color: red">Metroset </span>
+        - Board
+      </h2>
+    </v-row>
     <v-btn
       icon="mdi-login"
       :to="'/login'"
@@ -20,5 +23,3 @@
 </template>
 
 <script setup lang="ts"></script>
-
-<style lang="scss" scoped></style>
