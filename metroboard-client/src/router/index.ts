b16481b5c6cd49b4015ee492375ae4fccf76b9ec
posop <artur.yakhin52@gmail.com>
2023-03-13 20:34:30 +0500

    add router auth guard

--- metroboard-client/src/router/index.ts
+++ metroboard-client/src/router/index.ts
@@ -1,17 +1,42 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { store } from '@/store/store'
 import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/registration',
+    name: 'registration',
+    component: () => import('@/views/Registration.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from) => {
+  if (to.meta.requiresAuth && !store.getters.checkAuth) {
+    return {
+      path: '/login',
+      query: {
+        redirect: to.fullPath
+      }
+    }
+  }
 })
 
 export default router
