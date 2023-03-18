0ae92932cba4379d0288edf8fe55b9e7dfd61503
posop <artur.yakhin52@gmail.com>
2023-03-18 18:33:29 +0500

    refactoring form alerts

---
+++ metroboard-client/src/components/Alert/AuthFormAlert.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-alert
+    density="compact"
+    v-if="status"
+    type="error"
+    title="Error"
+    :text="status"
+  ></v-alert>
+</template>
+
+<script setup lang="ts">
+interface AuthFormAlertProps {
+  status: string
+}
+
+const { status } = defineProps<AuthFormAlertProps>()
+</script>
