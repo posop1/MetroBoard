ca72896790bcdcf0eb45f3534418e49a3aa20349
posop <artur.yakhin52@gmail.com>
2023-03-21 16:12:15 +0500

    rework data functions

--- metroboard-server/src/utils/getDataFile.ts
+++ metroboard-server/src/utils/getDataFile.ts
@@ -4,26 +4,6 @@ import { IColumn } from '../types/column'
 import { ITask } from '../types/task'
 import { IUser } from '../types/auth'
 
-// export const getColumnData = () => {
-//   const data = fs.readFileSync(path.join(__dirname, '../../data/columnData.json'), 'utf8')
-//   const column: IColumn[] = JSON.parse(data)
-
-//   return column
-// }
-
-// export const getTaskData = () => {
-//   const data = fs.readFileSync(path.join(__dirname, '../../data/taskData.json'), 'utf8')
-//   const task: ITask[] = JSON.parse(data)
-
-//   return task
-// }
-
-// export const getUserData = () => {
-//   const data = fs.readFileSync(path.join(__dirname, '../../data/userData.json'), 'utf8')
-//   const user: IUser[] = JSON.parse(data)
-
-//   return user
-// }
 
 export const getData = {
   column() {
--- metroboard-server/src/utils/writeDataFile.ts
+++ metroboard-server/src/utils/writeDataFile.ts
@@ -4,18 +4,6 @@ import { IColumn } from '../types/column'
 import { ITask } from '../types/task'
 import { IUser } from '../types/auth'
 
-// export const writeColumnData = (data: IColumn[]) => {
-//   fs.writeFileSync(path.join(__dirname, '../../data/columnData.json'), JSON.stringify(data))
-// }
-
-// export const writeTaskData = (data: ITask[]) => {
-//   fs.writeFileSync(path.join(__dirname, '../../data/taskData.json'), JSON.stringify(data))
-// }
-
-// export const writeUserData = (data: IUser[]) => {
-//   fs.writeFileSync(path.join(__dirname, '../../data/userData.json'), JSON.stringify(data))
-// }
-
 export const writeData = {
   column(data: IColumn[]) {
     fs.writeFileSync(path.join(__dirname, '../../data/columnData.json'), JSON.stringify(data))
