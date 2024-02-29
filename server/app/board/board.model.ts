c73410da498594b312df97c9546d352ac23f8a2d
posop <artur.yakhin52@gmail.com>
2024-02-29 20:21:08 +0500

    refactoring

--- server/app/board/board.model.ts
+++ server/app/board/board.model.ts
@@ -1,26 +1,26 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const BoardScheme = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  columns: {
-    type: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Column'
-      }
-    ]
-  },
-  tasks: {
-    type: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Task'
-      }
-    ]
-  }
-})
+	title: {
+		type: String,
+		required: true
+	},
+	columns: {
+		type: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Column"
+			}
+		]
+	},
+	tasks: {
+		type: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Task"
+			}
+		]
+	}
+});
 
-export const Board = mongoose.model('Board', BoardScheme)
+export const Board = mongoose.model("Board", BoardScheme);
